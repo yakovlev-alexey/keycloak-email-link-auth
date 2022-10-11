6c86bff1e73091901d021de1ee31d044410cfb74
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2022-10-11 16:29:40 +0300

    Initial commit

---
+++ src/main/java/dev/yakovlev_alexey/App.java
@@ -0,0 +1,11 @@
+package dev.yakovlev_alexey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
